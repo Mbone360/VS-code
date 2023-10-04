2cc9e3a23b7ecc488d7e88b0cc5019af0ab87b34
Mbone360 <rowan@mcfaul.ca>
2023-10-04 12:28:34 -0400

    test

---
+++ assignment-1/role-1.py
@@ -0,0 +1,9 @@
+'''
+    This module handles the attributes for the Space Marine class
+'''
+name1 = "Space Marine"
+strg1 = 10  # strength atribute
+dex1 = 6    # dexterity atribute
+intl = 4    # inteligence atribute
+hh1 = 500   # health atribute
+lv1 = 1     # lives atribute
---
+++ assignment-1/role-2.py
@@ -0,0 +1,9 @@
+'''
+    This module handles the attributes for the Tau class
+'''
+name2 = "Tau"
+strg2 = 3   # strength attribute
+dex2 = 8    # dexterity attribute
+int2 = 10   # inteligence attribute
+hh2 = 100   # health attribute
+lv2 = 5     # lives attribute
